df302db6bf861ac3d2120853ba400f2fe6e5f08b
Kenan Mammedli <kenan.mammedli1111@gmail.com>
2025-12-02 18:39:06 +0400

    Update consumer scripts, docker-compose, and application properties

--- consumer-script.sh
+++ consumer-script.sh
@@ -1,5 +1,3 @@
-sleep 10
-
 kafka-console-consumer \
   --bootstrap-server kafka:9092 \
   --topic pgcdc.public.actor \
@@ -7,13 +5,5 @@ kafka-console-consumer \
   --property print.key=false \
   --property print.timestamp=false \
   >> /output/actor_cdc.json &
-
-# kafka-console-consumer \
-#   --bootstrap-server kafka:9092 \
-#   --topic pgcdc.public.film \
-#   --from-beginning \
-#   --property print.key=false \
-#   --property print.timestamp=false \
-#   >> /output/film_cdc.json &
 
 tail -f /dev/null
